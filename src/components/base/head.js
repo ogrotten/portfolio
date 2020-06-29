c9c22e3f0d97b232f7f39010779115b202756b2b
chip <chiposaur@gmail.com>
2020-06-29 15:11:07 -0500

    files ok

--- src/components/base/head.js
+++ src/components/base/head.js
@@ -7,5 +7,19 @@ import {
 } from 'recoil';
 
 export default function HeadComp() {
-	return " "
+	return (
+		<header>
+			<nav>
+				<div className="logo">
+					<h1>Darrin Lowery</h1>
+					<p>Web Developer ● Design Analyst</p>
+				</div>
+				<div className="navlinks">
+                    <a className="button" alt="Skills" href="#skills">Skills</a>
+					<a className="button" alt="Projects" href="#projects">Projects</a>
+					<a className="button" alt="History" href="#history">Highlights</a>
+				</div>
+			</nav>
+		</header>
+	)
 }
